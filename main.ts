4daaa74c28742921dd5475a07ba20737481a5d8e
Juneral <30858934+Juneral@users.noreply.github.com>
2025-10-29 16:00:22 +0800

    update

--- main.ts
+++ main.ts
@@ -7,32 +7,59 @@ enum LED {
 
 //% color="#31C7D5" weight=10 icon="\uf2db"
 namespace Microbit_Kit {
+
+    //% blockId=my_digital_pin
+    //% block="digital pin $pin"
+    //% pin.fieldEditor=pinpicker
+    //% pin.fieldOptions.columns=4
+    //% pin.fieldOptions.tooltips="false"
+    //% weight=17
+    //% blockGap=8
+    //% advanced=true
+    //% decompilerShadowAlias=my_digital_pin_shadow
+    //% blockHidden=1
+    export function _digitalPin(pin: DigitalPin): number {
+        return pin;
+    }
+
+    //% blockId=my_analog_read_write_pin_shadow
+    //% block="$pin"
+    //% pin.fieldEditor=pinpicker
+    //% pin.fieldOptions.columns=4
+    //% pin.fieldOptions.tooltips="false"
+    //% blockHidden=1
+    export function _analogReadWritePinShadow(pin: AnalogReadWritePin): number {
+        return pin;
+    }
+
     //% blockId=SetLed block="LED at %pin| is %status"   group="LED"
     //% weight=70
-    export function myLED(pin: DigitalPin, status: LED): void {
+    //% pin.shadow=my_digital_pin_shadow
+    export function myLED(pin: number, status: LED): void {
         pins.digitalWritePin(pin, status)
     }
 
     //% blockId=Button block="Button at %pin| is pressed" 
     //% group="Button"
     //% weight=70
-    export function myButton(pin: DigitalPin): boolean {
+    //% pin.shadow=my_digital_pin_shadow
+    export function myButton(pin: number): boolean {
         return pins.digitalReadPin(pin)==0?true:false;
     }
 
     //% blockId=Light_Sensor block="Light Sensor at %pin| get value"
     //% group="Light Sensor"
+    //% pin.shadow=my_analog_read_write_pin_shadow
     //% weight=70
-    export function myLightSensor(pin: AnalogPin): number {
+    export function myLightSensor(pin: number): number {
         return pins.analogReadPin(pin)
     }
 
     //% blockId=Sound_Sensor block="Sound Sensor at %pin| get value"
     //% group="Sound Sensor"
-    //% pin.shadow=analog_read_write_pin_shadow
+    //% pin.shadow=my_analog_read_write_pin_shadow
     //% weight=70
-    export function mySoundSensor(pin: AnalogPin): number {
+    export function mySoundSensor(pin: number): number {
         return pins.analogReadPin(pin)
     }
-
 }
