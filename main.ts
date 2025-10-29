c1949b0ba8b021a0a1b7009613d56035e608c79d
Juneral <30858934+Juneral@users.noreply.github.com>
2025-10-29 15:26:24 +0800

    update

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
+enum LED {
+    //% block="ON"
+    ON = 1,
+    //% block="OFF"
+    OFF = 0
+}
+
 //% color="#31C7D5" weight=10 icon="\uf2db"
 namespace Microbit_Kit {
-
-    export enum LED {
-        //% block="ON"
-        ON = 1,
-        //% block="OFF"
-        OFF = 0
-    }
-
     //% blockId=SetLed block="LED at %pin| is %status"   group="LED"
     //% weight=70
     export function myLED(pin: DigitalPin, status: LED): void {
         pins.digitalWritePin(pin, status)
     }
 
-    //% blockId=Button block="Button at %pin| is pressed"   group="LED"
+    //% blockId=Button block="Button at %pin| is pressed" 
+    //% group="Button"
     //% weight=70
     export function myButton(pin: DigitalPin): boolean {
         return pins.digitalReadPin(pin)==0?true:false;
