dddb1bab0ada1aaff8fa6d2a839255d447d2c066
Juneral <30858934+Juneral@users.noreply.github.com>
2025-10-29 13:40:15 +0800

    update

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
+//% color="#31C7D5" weight=10 icon="\uf1d0"
+namespace JunBit {
 
+    export enum LED {
+        //% block="ON"
+        ON = 1,
+        //% block="OFF"
+        OFF = 0
+    }
+    //% blockId=SetLed block="LED at %pin| is %status"   group="LED"
+    //% weight=70
+    //% subcategory="Display"
+    export function SetLed(pin: DigitalPin, status: LED): void {
+        pins.digitalWritePin(pin, status)
+    }
+}
